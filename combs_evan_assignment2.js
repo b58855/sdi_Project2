e9beec96a4ae36a6a6cccbfb4712330692b35310
Evan <evan@Evans-MacBook-Pro.local>
2013-02-14 16:48:56 -0500

    added a random variable to the script

--- combs_evan_assignment2.js
+++ combs_evan_assignment2.js
@@ -17,6 +17,12 @@ var Write = function(message)
 	console.log(message);
 }
 
+var ChooseGeneral = function()
+{
+	var randomNumber = Math.round(Math.random());
+	return randomNumber;
+}
+
 var ColonelSpeachIntro = function(string1, string2)
 {
 	return (string1 + ' is with ' + string2 + ' formulating an attack plan.');
@@ -46,7 +52,7 @@ var ColonelSpeach = function(number, array)
 var Orders = function(number)
 {
 	var placements = ["right flank", "left flank", "center", "center", "reserve"];
-	var integer = number - 1
+	var integer = number - 1;
 	while(integer >= 0)
 	{
 		Write(colonels[integer] + ' you will take ' + battalions[integer] + ' up the ' + placements[integer]);
@@ -68,9 +74,10 @@ var Bravery = function(input1, input2)
 }
 
 //final output
-Write(ColonelSpeachIntro(generals[1], strategist));
+var chosen = ChooseGeneral();
+Write(ColonelSpeachIntro(generals[chosen], strategist));
 Write(ColonelSpeach(battalionSize, battalions));
 Write('It will be no easy feet to win, but I have complete faith in these men and complete faith in you. When I come out of that tent with or orders we will execute those orders, and we will destroy our enemy.');
 WhenToAttack(Bravery(colonels[1], colonels[4]));
-Write('With all five colonels in the tent, ' + generals[1] + ' begins to talk. "We are going to battle ' + day + ' colonels. This is a dangerous plan, but it is the only way we will win this battle');
+Write('With all five colonels in the tent, ' + generals[chosen] + ' begins to talk. "We are going to battle ' + day + ' colonels. This is a dangerous plan, but it is the only way we will win this battle');
 Write(Orders(colonels.length));
